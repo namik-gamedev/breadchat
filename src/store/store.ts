da912bf6040596641582a04db39fedddd39d9df0
Namik Guseinov <gnamik007@gmail.com>
2023-04-24 17:38:58 +0300

    removing chat reducer from store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './reducers/UserSlice';
 import { usersReducer } from './reducers/UsersSlice';
 import { globalReducer } from './reducers/GlobalSlice';
-import { chatReducer } from './reducers/ChatSlice';
 import { chatsReducer } from './reducers/ChatsSlice';
 
 export const store = configureStore({
    reducer: {
-      chat: chatReducer,
       chats: chatsReducer,
       user: userReducer,
       users: usersReducer,
