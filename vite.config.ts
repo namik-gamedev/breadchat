0844f64ea19910cb51bc86590f7c5905488022ea
Namik Guseinov <gnamik007@gmail.com>
2023-05-07 21:48:35 +0300

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/react-project/',
    plugins: [react()],
    resolve: {
       alias: {
