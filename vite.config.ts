addeedd30fc24a70f68fba719609e2a9f23d4449
Namik Guseinov <gnamik007@gmail.com>
2023-05-07 21:53:03 +0300

    4th commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/react-project/',
+   base: '/bread/',
    plugins: [react()],
    resolve: {
       alias: {
