d695ef7528aee16e9119e4f9dbda2f9dea6b1ecb
Namik Guseinov <gnamik007@gmail.com>
2023-05-08 18:03:03 +0300

    fix truncate overflow issue

--- src/utils/truncate.util.ts
+++ src/utils/truncate.util.ts
@@ -1,1 +1,1 @@
-export const truncate = (str: string, length: number) => (str.length > length ? `${str}...` : str);
+export const truncate = (str: string, length: number) => (str.length > length ? `${str.substring(0, length)}...` : str.substring(0, length));
