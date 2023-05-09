0d1fd91c677795f9f461d73f83cbe2812525bdb9
Namik Guseinov <gnamik007@gmail.com>
2023-05-09 21:41:21 +0300

    adding to notifications menu chats with unreaded messages

--- src/components/Header/NotificationsMenu/NotificationsMenu.tsx
+++ src/components/Header/NotificationsMenu/NotificationsMenu.tsx
@@ -8,6 +8,7 @@ import { useAnchorEl } from 'src/hooks/useAnchorEl';
 import Popover from '@mui/material/Popover';
 import { StyledMenu } from 'src/components/UI/StyledMenu';
 import { useAppSelector } from 'src/hooks/useAppSelector';
+import { ChatThumbnail } from 'src/pages/Chats/ChatThumbnail';
 
 export interface NotificationsMenuProps {}
 
@@ -15,16 +16,33 @@ export const NotificationsMenu: FC<NotificationsMenuProps> = ({}) => {
    const { anchorEl, open, handleShow, handleClose } = useAnchorEl();
 
    const chats = useAppSelector((state) => state.chats.data);
-   const notificationsCount = chats.reduce((total, chat) => total + chat.unreadedMessagesCount, 0);
+   const chatsWithUnreadedMessages = chats.filter((chat) => chat.unreadedMessagesCount > 0);
+   const totalUnreadedMessagesCount = chatsWithUnreadedMessages.reduce((total, chat) => total + chat.unreadedMessagesCount, 0);
 
    return (
       <Box>
          <IconButton onClick={handleShow}>
-            <Badge badgeContent={notificationsCount} color='info'>
+            <Badge badgeContent={totalUnreadedMessagesCount} color='info'>
                <NotificationsNoneIcon color='primary' />
             </Badge>
          </IconButton>
-         <Popover anchorEl={anchorEl} open={open} onClose={handleClose}></Popover>
+         <Popover
+            anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+            transformOrigin={{
+               horizontal: 'right',
+               vertical: 'top',
+            }}
+            sx={{ overflow: 'scroll', maxHeight: 200 }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+         >
+            {chatsWithUnreadedMessages.map((chat) => (
+               <Box sx={{ width: 300, p: 2 }}>
+                  <ChatThumbnail chat={chat} />
+               </Box>
+            ))}
+         </Popover>
       </Box>
    );
 };
