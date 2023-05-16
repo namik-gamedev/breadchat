5f08786c73f90e47f44f50bd45e56f5ec5ce91ed
Namik Guseinov <gnamik007@gmail.com>
2023-05-16 14:41:57 +0300

    import fixes

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,7 +1,7 @@
 import i18n, { Resource } from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import translationEn from 'src/translations/translation.en.json';
-import translationRu from 'src/translations/translation.ru.json';
+import translationEn from 'src/locales/translations/en.translation.json';
+import translationRu from 'src/locales/translations/ru.translation.json';
 
 const resources: Resource = {
    en: {
