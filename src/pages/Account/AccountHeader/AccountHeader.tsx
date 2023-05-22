c5fdf526b3a8c6913e5879b14e7f5cd6dfb0fb93
Namik Guseinov <gnamik007@gmail.com>
2023-05-22 16:14:18 +0300

    add avatar big view

--- src/pages/Account/AccountHeader/AccountHeader.tsx
+++ src/pages/Account/AccountHeader/AccountHeader.tsx
@@ -2,6 +2,8 @@ import React, { FC, useRef } from 'react';
 import { UserThumbnail } from 'src/components/UI/UserThumbnail';
 import { useAppSelector } from 'src/hooks/useAppSelector';
 import { IUser } from 'src/types/types';
+import Backdrop from '@mui/material/Backdrop';
+import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Input from '@mui/material/Input';
 import Box from '@mui/material/Box';
@@ -10,6 +12,10 @@ import ChatIcon from '@mui/icons-material/Chat';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import { UnstyledLink } from 'src/components/UI/UnstyledLink';
 import UserService from 'src/services/user.service';
+import { UserAvatar } from 'src/components/UI/UserAvatar';
+import moment from 'moment';
+import { Trans } from 'react-i18next';
+import { useOpen } from 'src/hooks/useOpen';
 
 export interface AccountHeaderProps {
    user: IUser;
@@ -18,6 +24,8 @@ export interface AccountHeaderProps {
 
 export const AccountHeader: FC<AccountHeaderProps> = ({ user, isCurrentUser }) => {
    const inputRef = useRef<HTMLInputElement | null>(null);
+
+   const { open, handleClose, handleShow } = useOpen();
 
    const handleClick = () => {
       inputRef.current?.click();
@@ -37,7 +45,21 @@ export const AccountHeader: FC<AccountHeaderProps> = ({ user, isCurrentUser }) =
 
    return (
       <Stack direction='row' sx={{ justifyContent: 'space-between' }}>
-         <UserThumbnail user={user} />
+         <Stack direction='row' spacing={2} alignItems='center'>
+            <Box onClick={handleShow}>
+               <UserAvatar sx={{ width: 50, height: 50, fontSize: '1.5em' }} user={user} />
+            </Box>
+
+            <Box>
+               <Typography variant='h6' sx={{ fontWeight: 'normal' }} component='h2'>
+                  {user.displayName}
+               </Typography>
+               <Typography variant='body1' sx={{ color: 'text.secondary' }}>
+                  <Trans values={{ time: moment(user.lastSeen).calendar() }}>{user.online ? 'online' : 'last seen {{time}}'}</Trans>
+               </Typography>
+            </Box>
+         </Stack>
+
          <Box>
             {isCurrentUser ? (
                <>
@@ -52,6 +74,10 @@ export const AccountHeader: FC<AccountHeaderProps> = ({ user, isCurrentUser }) =
                </IconButton>
             )}
          </Box>
+
+         <Backdrop sx={{ zIndex: 1100 }} open={open} onClick={handleClose}>
+            <Box component='img' src={user.photoURL || ''} sx={{ maxHeight: 1, maxWidth: 1 }} />
+         </Backdrop>
       </Stack>
    );
 };
