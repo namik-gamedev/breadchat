980e313647517d8bcfdd009df16003f6236773ff
Namik Guseinov <gnamik007@gmail.com>
2023-05-24 12:43:45 +0300

    add notifications badge in navigation menu

--- src/components/Header/HeaderNavigationMenu/HeaderNavigationMenu.tsx
+++ src/components/Header/HeaderNavigationMenu/HeaderNavigationMenu.tsx
@@ -1,17 +1,21 @@
 import React, { FC, useState } from 'react';
 import { useMediaQuery } from '@mui/material';
 import Box from '@mui/material/Box';
+import Badge from '@mui/material/Badge';
 import { NavigationMenuItems } from 'src/components/NavigationMenuItems';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { StyledMenu } from 'src/components/UI/StyledMenu';
 import { ProfileMenuItems } from 'src/components/ProfileMenuItems';
 import { useAnchorEl } from 'src/hooks/useAnchorEl';
+import { useAppSelector } from 'src/hooks/useAppSelector';
 
 export interface HeaderNavigationMenuProps {}
 
 export const HeaderNavigationMenu: FC<HeaderNavigationMenuProps> = ({}) => {
    const { anchorEl, open, handleShow, handleClose } = useAnchorEl();
+
+   const unreadedMessagesCount = useAppSelector((state) => state.chats.data).reduce((total, chat) => total + chat.unreadedMessagesCount, 0);
 
    return (
       <Box
@@ -23,7 +27,9 @@ export const HeaderNavigationMenu: FC<HeaderNavigationMenuProps> = ({}) => {
          }}
       >
          <IconButton onClick={handleShow}>
-            <MenuIcon color='primary' />
+            <Badge badgeContent={unreadedMessagesCount} variant='dot' color='info'>
+               <MenuIcon color='primary' />
+            </Badge>
          </IconButton>
          <StyledMenu anchorEl={anchorEl} open={open} onClose={handleClose}>
             <NavigationMenuItems handleClose={handleClose} />
--- src/components/NavigationMenuItems/NavigationMenuItems.tsx
+++ src/components/NavigationMenuItems/NavigationMenuItems.tsx
@@ -5,14 +5,19 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import GroupIcon from '@mui/icons-material/Group';
 import ForumIcon from '@mui/icons-material/Forum';
 import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
 import { UnstyledLink } from '../UI/UnstyledLink';
 import { Trans } from 'react-i18next';
+import { useAppSelector } from 'src/hooks/useAppSelector';
+import { UnreadedMessagesCountDisplay } from '../UI/UnreadedMessagesCountDisplay';
 
 export interface NavigationMenuItemsProps {
    handleClose?: () => void;
 }
 
 export const NavigationMenuItems: FC<NavigationMenuItemsProps> = ({ handleClose }) => {
+   const unreadedMessagesCount = useAppSelector((state) => state.chats.data).reduce((total, chat) => total + chat.unreadedMessagesCount, 0);
+
    return (
       <>
          <MenuItem component={UnstyledLink} to='/users' onClick={handleClose}>
@@ -26,7 +31,9 @@ export const NavigationMenuItems: FC<NavigationMenuItemsProps> = ({ handleClose 
 
          <MenuItem component={UnstyledLink} to='/chats' onClick={handleClose}>
             <ListItemIcon>
-               <ForumIcon color='primary' />
+               <Badge badgeContent={unreadedMessagesCount} variant='dot' color='info'>
+                  <ForumIcon color='primary' />
+               </Badge>
             </ListItemIcon>
             <Typography>
                <Trans>chats</Trans>
