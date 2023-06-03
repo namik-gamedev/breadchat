43f57d29658d45f0b2336669b499c1dc1e51d043
Namik Guseinov <gnamik007@gmail.com>
2023-06-03 15:35:06 +0300

    fix issue with translate

--- src/constants/AuthForm.consts.ts
+++ src/constants/AuthForm.consts.ts
@@ -1,7 +1,7 @@
 export const REQUIRED_FORM_ERR: string = 'field is required';
 export const INVALID_NAME_FORM_ERR: string = 'invalid name';
 export const INVALID_EMAIL_FORM_ERR: string = 'invalid email';
-export const INVALID_PASSWORD_FORM_ERR: string = 'invalid passowrd';
+export const INVALID_PASSWORD_FORM_ERR: string = 'invalid password';
 // 'Password must contain 8 and more characters: at least 1 lowercase, 1 uppercase letters and 1 digit!';
 export const CONFIRM_PASSWORD_FORM_ERR: string = 'passwords must match';
 
