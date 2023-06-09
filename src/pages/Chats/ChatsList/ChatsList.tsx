c4f1df2e265da9464674350ac667850cbf22d191
Namik Guseinov <gnamik007@gmail.com>
2023-06-09 21:49:52 +0300

    add pagination

--- src/pages/Chats/ChatsList/ChatsList.tsx
+++ src/pages/Chats/ChatsList/ChatsList.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import MenuList from '@mui/material/MenuList';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
@@ -7,25 +7,40 @@ import { UserThumbnail } from 'src/pages/Users/UserThumbnail';
 import { IChat, IUser } from 'src/types/types';
 import { useAppSelector } from 'src/hooks/useAppSelector';
 import { ChatThumbnail } from '../ChatThumbnail';
+import Pagination from '@mui/material/Pagination';
 import { NoChatsMessage } from '../NoChatsMessage';
 
 export interface ChatsListProps {
    chats: IChat[];
 }
 
+const CHATS_IN_PAGE = 20;
+
 export const ChatsList: FC<ChatsListProps> = ({ chats }) => {
+   const [page, setPage] = useState(1);
+
+   const handleChange = (e: React.ChangeEvent<unknown>, p: number) => {
+      setPage(p);
+   };
+
    return chats.length > 0 ? (
-      <MenuList>
-         <Stack>
-            {chats.map((chat) => (
+      <Box>
+         <MenuList>
+            {chats.slice((page - 1) * CHATS_IN_PAGE, (page - 1) * CHATS_IN_PAGE + CHATS_IN_PAGE).map((chat) => (
                <MenuItem divider>
                   <Box sx={{ width: 1 }}>
                      <ChatThumbnail chat={chat} />
                   </Box>
                </MenuItem>
             ))}
-         </Stack>
-      </MenuList>
+         </MenuList>
+
+         {chats.length > CHATS_IN_PAGE && (
+            <Stack sx={{ alignItems: 'center' }}>
+               <Pagination onChange={handleChange} count={Math.ceil(chats.length / CHATS_IN_PAGE)} />
+            </Stack>
+         )}
+      </Box>
    ) : (
       <NoChatsMessage />
    );
--- src/pages/Users/UsersList/UsersList.tsx
+++ src/pages/Users/UsersList/UsersList.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import MenuList from '@mui/material/MenuList';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import Pagination from '@mui/material/Pagination';
 import MenuItem from '@mui/material/MenuItem';
 import { UserThumbnail } from 'src/pages/Users/UserThumbnail';
 import { IUser } from 'src/types/types';
@@ -12,24 +13,40 @@ export interface UsersListProps {
    users: IUser[];
 }
 
+const USERS_IN_PAGE = 20;
+
 export const UsersList: FC<UsersListProps> = ({ users }) => {
    const user = useAppSelector((state) => state.user.data)!;
 
+   const [page, setPage] = useState(1);
+
+   const handleChange = (e: React.ChangeEvent<unknown>, p: number) => {
+      setPage(p);
+   };
+
    return users.length > 0 ? (
-      <MenuList>
-         <Box>
-            {users.map(
-               (u) =>
-                  user.uid !== u.uid && (
+      <Box>
+         <MenuList>
+            {users
+               .filter((u) => !(u.uid === user.uid))
+               .slice((page - 1) * USERS_IN_PAGE, (page - 1) * USERS_IN_PAGE + USERS_IN_PAGE)
+               .map((u) => {
+                  return (
                      <MenuItem divider>
                         <Box sx={{ width: 1 }}>
                            <UserThumbnail user={u} />
                         </Box>
                      </MenuItem>
-                  )
-            )}
-         </Box>
-      </MenuList>
+                  );
+               })}
+         </MenuList>
+
+         {users.length - 1 > USERS_IN_PAGE && (
+            <Stack sx={{ alignItems: 'center' }}>
+               <Pagination onChange={handleChange} count={Math.ceil((users.length - 1) / USERS_IN_PAGE)} />
+            </Stack>
+         )}
+      </Box>
    ) : (
       <NoUsersMessage />
    );
