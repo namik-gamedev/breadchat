d51fee8937cde4ff66cf24c8fb22cb4162f16482
Namik Guseinov <gnamik007@gmail.com>
2023-06-14 17:46:28 +0300

    apply context

--- src/pages/Users/UsersList/UsersList.tsx
+++ src/pages/Users/UsersList/UsersList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import MenuList from '@mui/material/MenuList';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
@@ -8,15 +8,15 @@ import { UserThumbnail } from 'src/pages/Users/UserThumbnail';
 import { IUser } from 'src/types/types';
 import { useAppSelector } from 'src/hooks/useAppSelector';
 import { NoUsersMessage } from '../NoUsersMessage';
+import { UsersContext } from '../Users';
 
-export interface UsersListProps {
-   users: IUser[];
-   searchQuery: string;
-}
+export interface UsersListProps {}
 
 const USERS_IN_PAGE = 20;
 
-export const UsersList: FC<UsersListProps> = ({ users, searchQuery }) => {
+export const UsersList: FC<UsersListProps> = ({}) => {
+   const { filteredUsers: users, searchQuery, usersShowType } = useContext(UsersContext);
+
    const [newUsers, setNewUsers] = useState(users);
 
    const [page, setPage] = useState(1);
@@ -25,18 +25,10 @@ export const UsersList: FC<UsersListProps> = ({ users, searchQuery }) => {
       setPage(p);
    };
 
-   useEffect(() => {
-      if (searchQuery === '') {
-         setNewUsers(users.filter((u) => u.online));
-      } else {
-         setNewUsers(users);
-      }
-   }, [users, searchQuery]);
-
-   return newUsers.length > 0 ? (
+   return users.length > 0 ? (
       <Box>
          <MenuList>
-            {newUsers.slice((page - 1) * USERS_IN_PAGE, (page - 1) * USERS_IN_PAGE + USERS_IN_PAGE).map((u) => {
+            {users.slice((page - 1) * USERS_IN_PAGE, (page - 1) * USERS_IN_PAGE + USERS_IN_PAGE).map((u) => {
                return (
                   <MenuItem divider>
                      <Box sx={{ width: 1 }}>
@@ -47,13 +39,13 @@ export const UsersList: FC<UsersListProps> = ({ users, searchQuery }) => {
             })}
          </MenuList>
 
-         {users.length - 1 > USERS_IN_PAGE && (
+         {users.length > USERS_IN_PAGE && (
             <Stack sx={{ alignItems: 'center' }}>
-               <Pagination onChange={handleChange} count={Math.ceil((users.length - 1) / USERS_IN_PAGE)} />
+               <Pagination onChange={handleChange} count={Math.ceil(users.length / USERS_IN_PAGE)} />
             </Stack>
          )}
       </Box>
    ) : (
-      <NoUsersMessage isSearchQueryEmpty={searchQuery === ''} />
+      <NoUsersMessage />
    );
 };
