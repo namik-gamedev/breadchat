75ac5613b283bfae10e9e4f8da9a68184951a3dc
Namik Guseinov <gnamik007@gmail.com>
2023-06-14 17:46:11 +0300

    создание метода getMessageText

--- src/pages/Users/NoUsersMessage/NoUsersMessage.tsx
+++ src/pages/Users/NoUsersMessage/NoUsersMessage.tsx
@@ -1,17 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import { Trans } from 'react-i18next';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { UsersContext } from '../Users';
+import { UsersShowType } from 'src/types/types';
 
-export interface NoUsersMessageProps {
-   isSearchQueryEmpty: boolean;
-}
+export interface NoUsersMessageProps {}
 
-export const NoUsersMessage: FC<NoUsersMessageProps> = ({ isSearchQueryEmpty }) => {
+const getMessageText = (searchQueryIsEmpty: boolean, usersShowType: UsersShowType): string => {
+   if (searchQueryIsEmpty) {
+      if (usersShowType === UsersShowType.ONLINE) {
+         return 'no users online';
+      }
+      return 'no users';
+   } else {
+      return 'no users found';
+   }
+};
+
+export const NoUsersMessage: FC<NoUsersMessageProps> = ({}) => {
+   const { searchQuery, usersShowType } = useContext(UsersContext);
+
    return (
-      <Stack sx={{ justifyContent: 'center', alignItems: 'center', height: 1 }}>
+      <Stack sx={{ alignItems: 'center', pt: 2 }}>
          <Typography variant='body1'>
-            <Trans>{isSearchQueryEmpty ? 'no users online' : 'no users found'}</Trans>
+            <Trans>{getMessageText(searchQuery === '', usersShowType)}</Trans>
          </Typography>
       </Stack>
    );
