73983483f51c67c7c99f5dc10d47f554c6f2cce7
Namik Guseinov <gnamik007@gmail.com>
2023-06-18 14:34:50 +0300

    fix google sign in

--- src/pages/Auth/SignInForm/GoogleButton/GoogleButton.tsx
+++ src/pages/Auth/SignInForm/GoogleButton/GoogleButton.tsx
@@ -6,16 +6,31 @@ import Typography from '@mui/material/Typography';
 import { FC } from 'react';
 import { useNavigate } from 'react-router';
 import { appAuth, authProvider } from 'src/firebase/firebase';
+import { useAppSelector } from 'src/hooks/useAppSelector';
+import UserService from 'src/services/user.service';
 
 export const GoogleButton: FC = () => {
    const navigate = useNavigate();
+   const users = useAppSelector((state) => state.users.data);
 
    const handleGoogleSignIn = async () => {
-      const { user }: { user: User } = await signInWithPopup(appAuth, authProvider);
-      if (!user) {
-         return;
+      try {
+         const { user }: { user: User } = await signInWithPopup(appAuth, authProvider);
+
+         if (!users.some((u) => u.uid === user.uid)) {
+            UserService.setup({
+               displayName: user.displayName!,
+               uid: user.uid,
+               photoURL: user.displayName,
+               online: true,
+               lastSeen: Date.now(),
+               blockedUsers: [],
+            });
+         }
+         navigate('/');
+      } catch (e: any) {
+         console.log('something went wrong: ', e);
       }
-      navigate('/');
    };
 
    return (
