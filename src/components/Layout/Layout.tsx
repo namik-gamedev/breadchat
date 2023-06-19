2b92182ffd2187a2bb68765f01f5958d85ea02af
Namik Guseinov <gnamik007@gmail.com>
2023-06-19 14:50:31 +0300

    some changes in styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -14,10 +14,10 @@ export const Layout: FC<Props> = ({ children }) => {
    const user = useAppSelector((state) => state.user.data);
 
    return (
-      <Stack direction='column' sx={{ height: 'calc(100vh - 65px)' }}>
+      <Stack direction='column' sx={{ height: '100vh' }}>
          <Header />
          <Container sx={{ height: 1 }}>
-            <Grid container spacing={1} sx={{ pt: 1, height: 1 }}>
+            <Grid container spacing={2} sx={{ pt: { md: 2, xs: 1 }, height: 1 }}>
                {user && (
                   <Grid
                      item
