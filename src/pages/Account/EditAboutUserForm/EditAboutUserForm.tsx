6f4c38e947ca67e153463209d4e67811a1184dfc
namik-gamedev <gnamik007@gmail.com>
2023-06-29 14:09:49 +0300

    remove unused imports

--- src/pages/Account/EditAboutUserForm/EditAboutUserForm.tsx
+++ src/pages/Account/EditAboutUserForm/EditAboutUserForm.tsx
@@ -1,10 +1,4 @@
-import CloseIcon from '@mui/icons-material/Close';
-import CreateIcon from '@mui/icons-material/Create';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
-import TextField from '@mui/material/TextField';
-import React, { FC, useState } from 'react';
+import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { EditForm } from 'src/components/UI/EditForm';
 import { useAccount } from 'src/hooks/useAccount';
